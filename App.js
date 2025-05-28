e4aae41b783521d01d1aefd1531fec1166068e63
Kay Yoon Mi <kaysinnanhtike@gmail.com>
2025-05-28 14:06:32 +0630

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Form from "./components/Form";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
